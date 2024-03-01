08121f7f34379ed452a20cfee445d9e55a0a7489
Breno Leal <110254486+Br3n0L@users.noreply.github.com>
2024-03-01 19:00:51 -0300

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("ATM CONSULTORIA"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                Image.asset('assets/images/logo.png'),
+                const Padding(padding: EdgeInsets.only(top: 32))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
